5e2d30f9c036fa3c2f57781d8dc724b0fc7cfd95
turtlelee <scott.lee.3898@gmail.com>
2017-05-18 23:01:22 +0200

    Fixed bolding

--- main.js
+++ main.js
@@ -38,31 +38,31 @@ var a,b=I.length;for(J={};--b>-1;)a=I[b],a&&a._lazy!==!1&&(a.render(a._lazy[0],a
 var RAFA={init:function(){intro.init()}};$(function(){RAFA.init()});var intro={getElements:function(){$blockLeft=$(".block--left"),$blockLeftBackground=$blockLeft.find(".block__bgi"),$blockLeftCtaWrapper=$blockLeft.find(".block__cta"),$blockLeftCta=$blockLeft.find(".block__cta a"),blockLeftCtaWidth=$blockLeftCta.width(),$blockLeftTitle=$blockLeft.find(".block__title:eq(0) span"),$blockLeftSocialIcon=$blockLeft.find(".block__social__link"),$blockLeftSub=$blockLeft.find(".block__title:eq(1) span"),$blockRight=$(".block--right"),$content=$(".content")},setElements:function(){TweenMax.set($blockLeft,{xPercent:-100}),TweenMax.set($blockLeftBackground,{xPercent:90}),TweenMax.set($blockLeftCta,{x:2*-blockLeftCtaWidth}),TweenMax.set($blockLeftTitle,{xPercent:-100,autoAlpha:0}),TweenMax.set($blockLeftSub,{xPercent:-100,autoAlpha:0}),TweenMax.set($blockLeftSocialIcon,{autoAlpha:0}),TweenMax.set($content,{y:5,autoAlpha:0})},slideIn:function(){var a=new TimelineMax;a.to($blockLeft,.1,{autoAlpha:1}).to($blockLeft,1.2,{xPercent:0,ease:Power4.easeInOut}).to($blockLeftBackground,0,{autoAlpha:1},"-= 1.2").to($blockLeftBackground,1.2,{autoAlpha:1,xPercent:0,ease:Power4.easeInOut},"-= 1.2").to($blockLeftTitle,.7,{xPercent:0,autoAlpha:1,ease:Power4.easeInOut,delay:.5},"-= 1.2").to($blockLeftSub,.7,{xPercent:0,autoAlpha:1,ease:Power4.easeInOut,delay:.55},"-= 1.15").to($blockLeftCta,.7,{x:0,ease:Power4.easeInOut,autoAlpha:1,delay:.55,onCompleteScope:$blockLeftCta,onComplete:intro.setEndState},"-= 1.1").staggerTo($blockLeftSocialIcon,.3,{autoAlpha:1},.05,"-= 0.1").staggerTo($content,.35,{y:0,autoAlpha:1,ease:Sine.easeOut},.1,"-= 0.05")},setEndState:function(){TweenMax.set($blockLeftCtaWrapper,{css:{overflow:"visible"}})},init:function(){intro.getElements(),intro.setElements(),intro.slideIn()}};
 
 // PRELOAD
-var images = new Array()
-function preload() {
-	for (i = 0; i < preload.arguments.length; i++) {
-		images[i] = new Image()
-		images[i].src = preload.arguments[i]
-	}
-}
-preload("img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg")
+// var images = new Array()
+// function preload() {
+// 	for (i = 0; i < preload.arguments.length; i++) {
+// 		images[i] = new Image()
+// 		images[i].src = preload.arguments[i]
+// 	}
+// }
+// preload("img/pic1.jpg", "img/pic2.jpg", "img/pic3.jpg")
 
 
-// CUSTOM
-$(window).on("scroll resize", function(){
-    var pos=$('.block--left').offset();
-    $('.content').each(function(){
-        if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top)
-        {
-        	var newPicName = "url('" + "img/" + $(this).attr('class').slice(-4) + ".jpg')";
-        	console.log(newPicName);
-        	$(".block__bgi").css('background-image', newPicName);
-            //$('#date').html($(this).html()); //or any other way you want to get the date
-            return; //break the loop
-        }
-    });
-});
+// // CUSTOM
+// $(window).on("scroll resize", function(){
+//     var pos=$('.block--left').offset();
+//     $('.content').each(function(){
+//         if(pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top)
+//         {
+//         	var newPicName = "url('" + "img/" + $(this).attr('class').slice(-4) + ".jpg')";
+//         	console.log(newPicName);
+//         	$(".block__bgi").css('background-image', newPicName);
+//             //$('#date').html($(this).html()); //or any other way you want to get the date
+//             return; //break the loop
+//         }
+//     });
+// });
 
-$(document).ready(function(){
-  $(window).trigger('scroll'); // init the value
-});
+// $(document).ready(function(){
+//   $(window).trigger('scroll'); // init the value
+// });
